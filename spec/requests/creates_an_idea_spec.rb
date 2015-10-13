2d4b63370d9b95d65da109e4d3b3d15377a443f1
Sebastian Abondano <sabondano1@gmail.com>
2015-10-12 22:19:32 -0600

    Add api endpoint that creates idea

---
+++ spec/requests/creates_an_idea_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "create an idea endpoint", type: :request do
+  it "creates an idea with valid parameters" do
+    params = { idea: { title:   "New Idea",
+                       body:    "the body of the idea",
+                       quality: "genius" }
+    }
+
+    post "/api/v1/ideas", params
+    body = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to            be_success
+    expect(response).to            have_http_status(200)
+    expect(body[:title]).to        eq(params[:idea][:title])
+    expect(body[:body]).to         eq(params[:idea][:body])
+    expect(body[:quality]).to      eq(params[:idea][:quality])
+    expect(Idea.first.title).to    eq(params[:idea][:title])
+    expect(Idea.first.body).to     eq(params[:idea][:body])
+    expect(Idea.first.quality).to  eq(params[:idea][:quality])
+  end
+end
